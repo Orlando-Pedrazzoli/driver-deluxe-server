145974b0a1b8dab329122aa483e596dec80ec469
Guilherme Carvalho <guicarv99@gmail.com>
2024-03-02 16:57:34 +0000

    routes changed to go fetch stuff on the front end

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const authRoutes = require('./routes/auth.routes');
 app.use('/auth', authRoutes);
 
 const serviceRoutes = require('./routes/services.routes');
-app.use('/services', serviceRoutes);
+app.use('/api', serviceRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
