fc08a0ecdc9d7773acf16943997b133daaad8991
Guilherme Carvalho <guicarv99@gmail.com>
2024-03-02 10:22:30 +0000

    login route complete

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const User = require('../models/User.model');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { isAuthenticated } = require('../middleware/jwt.middleware');
 
 const saltRounds = 10;
 
@@ -27,6 +29,14 @@ router.post('/signup', async (req, res, next) => {
         message:
           'Password needs at least 6 charactersand contain one number, one lowercase, one uppercase, and a special character',
       });
+    }
+
+    const licenseRegex = /\d{6}\/\d{4}/;
+
+    if (!licenseRegex.test(license)) {
+      return res
+        .status(400)
+        .json({ message: 'Provide a valid license number' });
     }
 
     const userExists = await User.findOne({ email });
@@ -63,6 +73,52 @@ router.post('/signup', async (req, res, next) => {
     console.log('Error creating the user', error);
     next(error);
   }
+});
+
+router.post('/login', async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    if (email === '' || password === '') {
+      return res.status(400).json({ message: 'All fields are mandatory' });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: 'Provided email is not registerd.' });
+    }
+
+    const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+
+    if (isPasswordCorrect) {
+      // create a payload for the JWT with the user info
+      // DO NOT SEND HASHED PASSWORD!!
+      const payload = { _id: user._id, email: user.email, name: user.name };
+
+      const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+        algorithm: 'HS256', // algorithm we wan to encrypt the token with
+        expiresIn: '6H', // time to live of the JWT
+      });
+      res.status(200).json({ authToken });
+    } else {
+      return res.status(401).json({ message: 'Unable to authenticate user' });
+    }
+  } catch (error) {
+    console.log('An error occured loging in the user', error);
+    next(error);
+  }
+});
+
+router.get('/verify', isAuthenticated, (req, res, next) => {
+  // if the jwt is valid, the payload gets decoded by the middleware
+  // and is made available in req.payload
+  console.log('req.payload', req.payload);
+
+  // send it back with the user data from the token
+  res.status(200).json(req.payload);
 });
 
 module.exports = router;
